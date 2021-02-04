63e7520ff9d6d3f76e806696b46bdb5f8dbafbc2
JokusPokus <jakob.schmitt@code.berlin>
2021-02-04 19:41:48 +0100

    change settings and delete statics

--- homepage/settings.py
+++ homepage/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k1#=xgxuigwyvb0v@f6^z_#%zhot!@(qzx1k)$9#d3ews&4*_-'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'k1#=xgxuigwyvb0v@f6^z_#%zhot!@(qzx1k)$9#d3ews&4*_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') or True
 
 ALLOWED_HOSTS = ['jokuspokus.com', 'www.jokuspokus.com', 'localhost']
 
