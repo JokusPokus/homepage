97e251d1e1837f27b0a9635c6550dcfd0c800513
JokusPokus <jakob.schmitt@code.berlin>
2021-05-15 18:07:49 +0200

    test: Add more UI tests

--- tests/UI/tests.py
+++ tests/UI/tests.py
@@ -1,17 +1,65 @@
-from django.test import LiveServerTestCase
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.management import call_command
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-class NimTest(LiveServerTestCase):
+class NimTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome("tests/UI/chromedriver.exe")
 
     def tearDown(self) -> None:
-        self.driver.close()
+        self.driver.quit()
 
-    def test_landing_page_link_is_displayed(self):
-        self.driver.get('http://127.0.0.1:8000/')
+    def test_nim_link_on_landing_page_is_displayed(self):
+        self.driver.get(self.live_server_url)
         nim_container = self.driver.find_element_by_id("nimAI-container")
         self.assertEqual(nim_container.find_element_by_tag_name("h2").text, "Beat my Nim AI!")
 
+    def test_landing_page_link_leads_to_nim_page(self):
+        self.driver.get(self.live_server_url)
+        nim_container = self.driver.find_element_by_id("nimAI-container")
+        nim_container.click()
+        self.assertEqual(self.driver.find_element_by_id("welcome").text, "Welcome to the game!")
+
+    def test_rules_are_toggled_on_button_click(self):
+        self.driver.get(self.live_server_url + "/showcase/nimAI/")
+        rules = self.driver.find_element_by_id("rules__text")
+        self.assertFalse(rules.is_displayed())
+
+        rules_button = self.driver.find_element_by_id("rules__btn")
+        rules_button.click()
+        self.assertTrue(rules.is_displayed())
+
+        rules_button.click()
+        self.assertFalse(rules.is_displayed())
+
+    def test_accepting_challenge_leads_to_training_screen(self):
+        self.driver.get(self.live_server_url + "/showcase/nimAI/")
+        challenge_accepted_button = self.driver.find_element_by_id("challenge-accepted")
+        challenge_accepted_button.click()
+
+        self.assertEqual(self.driver.find_element_by_id("message-display").text, "Let the AI learn the game!")
+
+    def test_input_slider_with_difficulty_label(self):
+        self.driver.get(self.live_server_url + "/showcase/nimAI/train/")
+        slider = self.driver.find_element_by_class_name("train-slider")
+        difficulty = self.driver.find_element_by_class_name("level")
+        diff_display = self.driver.find_element_by_id("value-display")
+
+        self.assertEqual(diff_display.text, "1")
+        self.assertEqual(difficulty.text, "EASY PEASY")
+
+        for _ in range(20):
+            slider.send_keys(Keys.RIGHT)
+
+        self.assertEqual(diff_display.text, "21")
+        self.assertEqual(difficulty.text, "A LIL' CHALLENGE")
+
+        for _ in range(279):
+            slider.send_keys(Keys.RIGHT)
+
+        self.assertEqual(diff_display.text, "10000")
+        self.assertEqual(difficulty.text, "PREPARE TO CRY")
