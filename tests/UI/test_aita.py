b4cd7c9ccbe937d48bbf65b2fb1ed4e4c8a14c60
JokusPokus <jakob.schmitt@code.berlin>
2021-05-15 21:49:20 +0200

    doc(test): Add documentation to AITA test

--- tests/UI/test_aita.py
+++ tests/UI/test_aita.py
@@ -15,19 +15,35 @@ class AITATest(StaticLiveServerTestCase):
         self.driver.quit()
 
     @patch("AITA.views.Post", autospec=True)
-    def test_feedback_to_verdict_is_correct(self, mock_post):
-        # Database mocking
+    def test_feedback_to_verdict_is_correct(self, post_stub):
+        """GIVEN a user on the post detail page of the AITA project and
+        a post with the community verdict 'asshole'
+
+        WHEN the user clicks the Asshole button
+
+        THEN the screen becomes green and a success feedback is shown.
+        """
+        # GIVEN
+        # Model stub (s.t. database isn't hit)
         post = Post.objects.create(title="Title", body="Lorem ipsum dolor...", is_asshole=1)
-        mock_post.objects.all.return_value = [post]
+        post_stub.objects.all.return_value = [post]
 
         self.driver.get(self.live_server_url + "/showcase/AITA/post/")
-
         body = self.driver.find_element_by_tag_name("body")
+
+        # Guard assertion: Background is white at first
         self.assertEqual(body.value_of_css_property("background-color"), "rgba(255, 255, 255, 1)")
 
         asshole_button = self.driver.find_element_by_id("asshole")
+
+        # WHEN
         asshole_button.click()
         time.sleep(1.1)
-        self.assertEqual(body.value_of_css_property("background-color"), "rgba(61, 153, 112, 1)")
+
+        # THEN
+        self.assertEqual(
+            body.value_of_css_property("background-color"),
+            "rgba(61, 153, 112, 1)"
+        )
         self.assertTrue(body.find_element_by_class_name("feedback-correct").is_displayed())
         self.assertFalse(body.find_element_by_class_name("feedback-incorrect").is_displayed())
--- tests/UI/test_nimAI.py
+++ tests/UI/test_nimAI.py
@@ -13,36 +13,57 @@ class NimTest(StaticLiveServerTestCase):
         self.driver.quit()
 
     def test_nim_link_on_landing_page_is_displayed(self):
+        # WHEN user goes to landing page
         self.driver.get(self.live_server_url)
+
+        # THEN NimAI info container is displayed
         nim_container = self.driver.find_element_by_id("nimAI-container")
         self.assertEqual(nim_container.find_element_by_tag_name("h2").text, "Beat my Nim AI!")
 
     def test_landing_page_link_leads_to_nim_page(self):
+        # GIVEN the NimAI info container on the landing page
         self.driver.get(self.live_server_url)
         nim_container = self.driver.find_element_by_id("nimAI-container")
+
+        # WHEN a user clicks on the container
         nim_container.click()
+
+        # THEN the NimAI index is displayed
         self.assertEqual(self.driver.find_element_by_id("welcome").text, "Welcome to the game!")
 
     def test_rules_are_toggled_on_button_click(self):
+        # GIVEN the NimAI landing page containing the rules button
         self.driver.get(self.live_server_url + "/showcase/nimAI/")
         rules = self.driver.find_element_by_id("rules__text")
         self.assertFalse(rules.is_displayed())
 
         rules_button = self.driver.find_element_by_id("rules__btn")
+
+        # WHEN the rules button is clicked
         rules_button.click()
+
+        # THEN the rules are shown
         self.assertTrue(rules.is_displayed())
 
+        # WHEN the button is clicked again
         rules_button.click()
+
+        # THEN the rules are hidden
         self.assertFalse(rules.is_displayed())
 
     def test_accepting_challenge_leads_to_training_screen(self):
+        # GIVEN the NimAI landing page with a "Challenge Accepted" button
         self.driver.get(self.live_server_url + "/showcase/nimAI/")
         challenge_accepted_button = self.driver.find_element_by_id("challenge-accepted")
+
+        # WHEN the button is clicked
         challenge_accepted_button.click()
 
+        # THEN the user is forwarded to the training screen
         self.assertEqual(self.driver.find_element_by_id("message-display").text, "Let the AI learn the game!")
 
     def test_input_slider_with_difficulty_label(self):
+        # GIVEN the training screen with a difficulty slider on level "EASY PEASY"
         self.driver.get(self.live_server_url + "/showcase/nimAI/train/")
         slider = self.driver.find_element_by_class_name("train-slider")
         difficulty = self.driver.find_element_by_class_name("level")
@@ -51,14 +72,19 @@ class NimTest(StaticLiveServerTestCase):
         self.assertEqual(diff_display.text, "1")
         self.assertEqual(difficulty.text, "EASY PEASY")
 
+        # WHEN the slider is moved 20 units to the right
         for _ in range(20):
             slider.send_keys(Keys.RIGHT)
 
+        # THEN the new level is "A LIL' CHALLENGE"
         self.assertEqual(diff_display.text, "21")
         self.assertEqual(difficulty.text, "A LIL' CHALLENGE")
 
+        # WHEN the slider is moved to the max value of 300
         for _ in range(279):
             slider.send_keys(Keys.RIGHT)
 
+        # THEN the level is "PREPARE TO CRY" and the difficulty is 10000
+        # due to the non-linear difficulty scale.
         self.assertEqual(diff_display.text, "10000")
         self.assertEqual(difficulty.text, "PREPARE TO CRY")
